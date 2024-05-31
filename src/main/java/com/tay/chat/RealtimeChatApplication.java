c9ccbfe40235bcd09f639f15da5d7ec6143d4fe8
tay <phuonggteyy@gmail.com>
2024-05-31 17:25:50 +0700

    first commit

---
+++ src/main/java/com/tay/chat/RealtimeChatApplication.java
@@ -0,0 +1,13 @@
+package com.tay.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeChatApplication.class, args);
+	}
+
+}
